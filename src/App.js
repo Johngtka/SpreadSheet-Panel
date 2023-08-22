adeec13921db413b0534dd68c5cd7c8737644ae1
Johngtka <johngtka@gmail.com>
2023-08-22 12:53:59 +0200

    style: prettier code

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import React from 'react';
+import React from "react";
 import "./index.css";
 
 class App extends React.Component {
   state = {
     mode: 0,
-    result: ''
-  }
+    result: "",
+  };
   calc() {
-    var res
-    var l1 = document.querySelector('#l1').value
-    var l2 = document.querySelector('#l2').value
-    const req = document.querySelector('#wynik')
-    l1 = l1.replaceAll(' ', '')
-    l2 = l2.replaceAll(' ', '')
+    var res;
+    var l1 = document.querySelector("#l1").value;
+    var l2 = document.querySelector("#l2").value;
+    const req = document.querySelector("#wynik");
+    l1 = l1.replaceAll(" ", "");
+    l2 = l2.replaceAll(" ", "");
     if (!!l1 && !!l2) {
-      l1 = parseInt(l1)
-      l2 = parseInt(l2)
+      l1 = parseInt(l1);
+      l2 = parseInt(l2);
       switch (this.state.mode) {
         case 0:
-          res = l1 + l2
-          break
+          res = l1 + l2;
+          break;
         case 1:
-          res = l1 - l2
-          break
+          res = l1 - l2;
+          break;
         case 2:
-          res = l1 * l2
-          break
+          res = l1 * l2;
+          break;
         case 3:
           if (l1 === 0 || l2 === 0) {
-            res = "Nie można"
+            res = "Nie można";
           } else {
-            res = l1 / l2
+            res = l1 / l2;
           }
-          break
+          break;
         case 4:
-          const meme = [l1, l2]
+          const meme = [l1, l2];
           res = meme.map((number) => <li class="meme">{number}</li>);
-          break
+          break;
         default:
-          break
+          break;
       }
       this.setState({
-        result: res
-      })
+        result: res,
+      });
     } else {
       req.innerHTML = '<i id="err">no value in any of the inputs</i>';
     }
@@ -50,27 +50,128 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header>React Calculator<br></br>Basic Calculator written in React</header>
+        <header>
+          React Calculator<br></br>Basic Calculator written in React
+        </header>
         <main>
           <div id="screen">
             <div id="do">
               <input type="text" id="l1" placeholder="tu 1 liczba"></input>
-              {this.state.mode === 0 && <div style={{ width: 40, height: 20, display: 'inline', fontFamily: "serif" }}>+</div>}
-              {this.state.mode === 1 && <div style={{ width: 40, height: 20, display: 'inline', fontFamily: "serif" }}>-</div>}
-              {this.state.mode === 2 && <div style={{ width: 40, height: 20, display: 'inline', fontFamily: "serif" }}>*</div>}
-              {this.state.mode === 3 && <div style={{ width: 40, height: 20, display: 'inline', fontFamily: "serif" }}>/</div>}
-              {this.state.mode === 4 && <div style={{ width: 40, height: 20, display: 'inline', fontFamily: "serif" }}>;-)</div>}
-              <input type="text" id="l2" placeholder="tu 2 liczba"></input><br></br><br></br>
-              <button className='dzial' onClick={this.calc.bind(this)}>=</button><br></br><br></br>
+              {this.state.mode === 0 && (
+                <div
+                  style={{
+                    width: 40,
+                    height: 20,
+                    display: "inline",
+                    fontFamily: "serif",
+                  }}
+                >
+                  +
+                </div>
+              )}
+              {this.state.mode === 1 && (
+                <div
+                  style={{
+                    width: 40,
+                    height: 20,
+                    display: "inline",
+                    fontFamily: "serif",
+                  }}
+                >
+                  -
+                </div>
+              )}
+              {this.state.mode === 2 && (
+                <div
+                  style={{
+                    width: 40,
+                    height: 20,
+                    display: "inline",
+                    fontFamily: "serif",
+                  }}
+                >
+                  *
+                </div>
+              )}
+              {this.state.mode === 3 && (
+                <div
+                  style={{
+                    width: 40,
+                    height: 20,
+                    display: "inline",
+                    fontFamily: "serif",
+                  }}
+                >
+                  /
+                </div>
+              )}
+              {this.state.mode === 4 && (
+                <div
+                  style={{
+                    width: 40,
+                    height: 20,
+                    display: "inline",
+                    fontFamily: "serif",
+                  }}
+                >
+                  ;-)
+                </div>
+              )}
+              <input type="text" id="l2" placeholder="tu 2 liczba"></input>
+              <br></br>
+              <br></br>
+              <button className="dzial" onClick={this.calc.bind(this)}>
+                =
+              </button>
+              <br></br>
+              <br></br>
             </div>
           </div>
-          <button className='dzial' onClick={() => this.setState({ mode: 0 })}>+</button>
-          <button className='dzial' onClick={() => this.setState({ mode: 1 })}>-</button><br></br>
-          <button className='dzial' onClick={() => this.setState({ mode: 2 })}>*</button>
-          <button className='dzial' onClick={() => this.setState({ mode: 3 })}>/</button><br></br>
-          <button className='dzial' onClick={() => this.setState({ mode: 4 })}>;-)</button><br></br>
-          <div style={{ fontSize: 40, backgroundColor: "#ffffff", height: 50, marginLeft: "auto", marginRight: "auto", marginTop: 20, borderTop: '2px solid #000000', borderBottom: '2px solid #000000' }} id="wynik">{this.state.result}</div><br></br>
-          <p style={{ margin: 0 }}><span style={{ color: '#ff0000' }}><b>*</b></span> - <i><b>" Wpisz liczbę 1 i 2 wybierz działanie<br></br> i kliknij = "</b></i></p>
+          <button className="dzial" onClick={() => this.setState({ mode: 0 })}>
+            +
+          </button>
+          <button className="dzial" onClick={() => this.setState({ mode: 1 })}>
+            -
+          </button>
+          <br></br>
+          <button className="dzial" onClick={() => this.setState({ mode: 2 })}>
+            *
+          </button>
+          <button className="dzial" onClick={() => this.setState({ mode: 3 })}>
+            /
+          </button>
+          <br></br>
+          <button className="dzial" onClick={() => this.setState({ mode: 4 })}>
+            ;-)
+          </button>
+          <br></br>
+          <div
+            style={{
+              fontSize: 40,
+              backgroundColor: "#ffffff",
+              height: 50,
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: 20,
+              borderTop: "2px solid #000000",
+              borderBottom: "2px solid #000000",
+            }}
+            id="wynik"
+          >
+            {this.state.result}
+          </div>
+          <br></br>
+          <p style={{ margin: 0 }}>
+            <span style={{ color: "#ff0000" }}>
+              <b>*</b>
+            </span>{" "}
+            -{" "}
+            <i>
+              <b>
+                " Wpisz liczbę 1 i 2 wybierz działanie<br></br> i kliknij = "
+              </b>
+            </i>
+          </p>
         </main>
       </div>
     );
